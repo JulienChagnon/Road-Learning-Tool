9b735197929b87dd0de673239a5a4dab39c95abb
JulienChagnon <julienchagnon9@gmail.com>
2026-01-20 12:08:27 -0500

    Added Matomo Tag Manager in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,5 +21,30 @@ export default function App() {
     };
   }, []);
 
+  useEffect(() => {
+    if (document.getElementById("matomo-tag-manager")) {
+      return;
+    }
+
+    const _mtm = ((window as unknown as { _mtm?: unknown[] })._mtm =
+      (window as unknown as { _mtm?: unknown[] })._mtm || []);
+    _mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
+
+    const d = document;
+    const g = d.createElement("script");
+    const s = d.getElementsByTagName("script")[0];
+
+    g.async = true;
+    g.id = "matomo-tag-manager";
+    g.src =
+      "https://cdn.matomo.cloud/road-learning.matomo.cloud/container_EF0mnvWM.js";
+
+    if (s?.parentNode) {
+      s.parentNode.insertBefore(g, s);
+    } else {
+      d.head?.appendChild(g);
+    }
+  }, []);
+
   return <MapView />;
 }
